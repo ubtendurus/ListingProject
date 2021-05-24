ce593951461636d3099700b3d424bebba5bac79f
Ugur Burak <utendurus@gmail.com>
2021-05-24 00:07:47 -0400

    On branch master  Initial commit

---
+++ app/Http/Controllers/RegisterStepTwoController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\City;
+use Illuminate\Http\Request;
+
+class RegisterStepTwoController extends Controller
+{
+    public function create()
+    {
+        $cities = City::all();
+
+        return view('auth.register-step2', compact(var_name:'cities'));
+    }
+
+    public function store(Request $request)
+    {
+        auth()->user()->update([
+            'phone' => $request->phone,
+            'address' => $request->address,
+            'city_id' => $request->city_id,
+        ]);
+
+        return redirect()->route('dashboard');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::group(['middleware' => ['auth','verified']], function(){
+    Route::group(['middleware' => ['registration_completed']], function(){
+        Route::get('/dashboard', function () {
+            return view('dashboard');
+        })->name('dashboard');
+    });
+
+
+    Route::get('register-step2',
+        [\App\Http\Controllers\RegisterStepTwoController::class, 'create'])
+        ->name('register-step2.create');;
+    Route::post('register-step2',
+        [\App\Http\Controllers\RegisterStepTwoController::class, 'store'])
+        ->name('register-step2.post');
+});
